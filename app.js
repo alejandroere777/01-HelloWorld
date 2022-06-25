f2adfec141de250831b52db77900f1f47244966d
alejandroere777 <alejandroere_777@hotmail.com>
2022-06-25 13:48:47 -0300

    2do cambio

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Hola mundo')
+// const app = require('express')();
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    // res.send('Hola mundo desde express')
+    res.status(200)
+    res.json({
+        hola: 'mundo'
+    })
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+    res.json({
+        msj: `Hola ${data.user}`
+    })
+
+})
+
+app.listen(3000, () => {
+    console.log('Express server started at port 3000')
+})
